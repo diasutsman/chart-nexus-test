066aec291e2e386016b6c643b03963bc3fda8ec6
Dias Utsman <utsmand91@gmail.com>
2024-06-04 14:57:52 +0700

    "Removed and added various CSS classes and utilities in output.css, and updated darkMode property in tailwind.config.js"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
-    darkMode: 'selector',
+	darkMode: "selector",
 	theme: {
 		extend: {
 			keyframes: {
